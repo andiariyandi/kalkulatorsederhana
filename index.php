1bd06364f2dc3b086d8dfcefa84e8028fb0498a8
Andi Ariyandi <andiariyandi24@gmail.com>
2021-07-13 07:49:39 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Program Kalkulator</title>
+	<link rel="stylesheet" type="text/css" href="style.css"> 
+    <!-- //memanggil file css -->
+</head>
+<body>
+    <!-- //logika untuk memasukkan variable bilangan 1 dan 2 serta memilih operator perkaliangan dengan inputan -->
+	<?php 
+	if(isset($_POST['hitung'])){
+		$bil1 = $_POST['bil1'];
+		$bil2 = $_POST['bil2'];
+		$operasi = $_POST['operasi'];
+		switch ($operasi) {
+			case 'tambah':
+				$hasil = $bil1+$bil2;
+			break;
+			case 'kurang':
+				$hasil = $bil1-$bil2;
+			break;
+			case 'kali':
+				$hasil = $bil1*$bil2;
+			break;
+			case 'bagi':
+				$hasil = $bil1/$bil2;
+			break;			
+		}
+	}
+	?>
+
+    <!-- //mengatur tampilan kalkulator -->
+	<div class="kalkulator">
+		<h2 class="judul">Program Kalkulator</h2>
+		<form method="post" action="index.php">		
+            <!-- //memnuat form imputan sesuai variabel	 -->
+			<input type="text" name="bil1" class="bil" autocomplete="off" placeholder="Masukkan Bilangan Pertama">
+			<input type="text" name="bil2" class="bil" autocomplete="off" placeholder="Masukkan Bilangan Kedua">
+			<select class="opt" name="operasi">
+				<option value="tambah">+</option>
+				<option value="kurang">-</option>
+				<option value="kali">x</option>
+				<option value="bagi">/</option>
+			</select>
+			<input type="submit" name="hitung" value="Hitung" class="tombol">											
+		</form>
+		<?php if(isset($_POST['hitung'])){ ?>
+			<input type="text" value="<?php echo $hasil; ?>" class="bil">
+		<?php }else{ ?>
+			<input type="text" value="0" class="bil">
+		<?php } ?>			
+	</div>
+</body>
+</html>
